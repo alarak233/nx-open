242f69f7bbc2977d145c519be80c6b399674ca9d
alarak233 <727072895@qq.com>
2020-11-25 14:42:54 +0800

    求自由曲面需求后角框架

---
+++ clearanceAngle/clearanceAngleCheck/clearanceAngleCheck/clearanceAngleCheck.c
@@ -0,0 +1,92 @@
+/*****************************************************************************
+**
+** clearanceAngleCheck.c
+**
+** Description:
+**     Contains Unigraphics entry points for the application.
+**
+*****************************************************************************/
+
+/* Include files */
+#include <stdarg.h>
+#include <stdio.h>
+#include <uf.h>
+#include <uf_ui_types.h>
+#include <uf_ui.h>
+
+static void ECHO(char *format, ...)
+{
+    char msg[UF_UI_MAX_STRING_BUFSIZE];
+    va_list args;
+    va_start(args, format);
+    vsnprintf_s(msg, sizeof(msg), _TRUNCATE, format, args);
+    va_end(args);
+    UF_UI_open_listing_window();
+    UF_UI_write_listing_window(msg);
+    UF_print_syslog(msg, FALSE);
+}
+
+#define UF_CALL(X) (report_error( __FILE__, __LINE__, #X, (X)))
+
+static int report_error( char *file, int line, char *call, int irc)
+{
+    if (irc)
+    {
+        char err[133];
+
+        UF_get_fail_message(irc, err);
+        ECHO("*** ERROR code %d at line %d in %s:\n",
+            irc, line, file);
+        ECHO("+++ %s\n", err);
+        ECHO("%s;\n", call);
+    }
+
+    return(irc);
+}
+
+
+/*****************************************************************************
+**  Activation Methods
+*****************************************************************************/
+/*  Explicit Activation
+**      This entry point is used to activate the application explicitly, as in
+**      "File->Execute UG/Open->User Function..." */
+extern DllExport void ufusr( char *parm, int *returnCode, int rlen )
+{
+    /* Initialize the API environment */
+    if( UF_CALL(UF_initialize()) ) 
+    {
+        /* Failed to initialize */
+        return;
+    }
+    
+    /* TODO: Add your application code here */
+
+	/*1.选取目标面*/
+
+	/*2.选取中心点*/
+
+	/*3.选取切削方向（顺时针，逆时针）及点的分布（度数，半径上点的个数）*/
+
+	/*4.生成点的法向量并对其进行分解得到后角值得到最大后角*/
+
+	/*5.后角可视化（生成对应点的后角生成的直线集合）*/
+
+    /* Terminate the API environment */
+    UF_CALL(UF_terminate());
+}
+
+/*****************************************************************************
+**  Utilities
+*****************************************************************************/
+
+/* Unload Handler
+**     This function specifies when to unload your application from Unigraphics.
+**     If your application registers a callback (from a MenuScript item or a
+**     User Defined Object for example), this function MUST return
+**     "UF_UNLOAD_UG_TERMINATE". */
+extern int ufusr_ask_unload( void )
+{
+    return( UF_UNLOAD_IMMEDIATELY );
+}
+
